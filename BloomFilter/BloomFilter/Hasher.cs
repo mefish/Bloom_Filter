2fa0a0e6fbe8ca6382959b488a834e8d215dca91
mefish <matt.e.fisher@gmail.com>
2017-01-08 18:03:18 -0500

    I don't have a lot of experience with bit operations and hashing outside of passwords and frequently used things, so I've implemented my own to get a better understanding of how all of this is fitting together, and give me a little more control over what's happening.  Also, we shouldn't be using cryptographic hashes for this anyway, there's no reason to since it's not for security purposes

---
+++ BloomFilter/BloomFilter/Hasher.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BloomFilterApp
+{
+    public class Hasher
+    {
+        //Shamelessly ripped from https://gist.github.com/richardkundl/8300092
+        public int Hash(string cat)
+        {
+            int hash = 0;
+
+            for (int i = 0; i < cat.Length; i++)
+            {
+                hash += cat[i];
+                hash += (hash << 10);
+                hash ^= (hash >> 6);
+            }
+
+            hash += (hash << 3);
+            hash ^= (hash >> 11);
+            hash += (hash << 15);
+            return hash;
+        }
+    }
+}
---
+++ BloomFilter/BloomFilterTests/HasherUnitTests.cs
@@ -0,0 +1,55 @@
+﻿using BloomFilterApp;
+using NUnit.Framework;
+
+namespace BloomFilterTests
+{
+    [TestFixture]
+    internal class HasherUnitTests
+    {
+        [Test]
+        public void AStringCanBeHashed()
+        {
+            var hasher = new Hasher();
+
+            var hash = hasher.Hash("Cat");
+
+            Assert.Less(0, hash);
+        }
+
+        [Test]
+        public void AStringIsHashedTheSameEveryTime()
+        {
+            var hasher = new Hasher();
+
+            var hash = hasher.Hash("Cat");
+            var secondHash = hasher.Hash("Cat");
+
+            Assert.AreEqual(hash, secondHash);
+        }
+
+        [Test]
+        public void DifferentStringsCreateDifferentHashes()
+        {
+            var hasher = new Hasher();
+
+            var hash = hasher.Hash("Cat");
+            var secondHash = hasher.Hash("Dog");
+
+            Assert.AreNotEqual(hash, secondHash);
+        }
+
+        [Test]
+        public void NoTwoHashesAreTheSameWithinReason()
+        {
+            var hasher = new Hasher();
+
+            var hash = hasher.Hash("Cat");
+            for (var i = 0; i < 1000; i++)
+            {
+                var randomString = BloomFilterTestHelpers.GetRandomString(3);
+                var randomStringHash = hasher.Hash(randomString);
+                if (randomString != "Cat") Assert.AreNotEqual(randomStringHash, hash);
+            }
+        }
+    }
+}
